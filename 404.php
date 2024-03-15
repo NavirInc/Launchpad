0b9488669f663866c08c2c5a80bc7b7cc08d8a06
foxtrotLi <58943731+foxtrotLi@users.noreply.github.com>
2024-03-15 08:58:26 -0400

    Add comment to 404.php

--- 404.php
+++ 404.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * The template for displaying 404 (Not Found) errors. This file is displayed
+ * when a requested page is not found on the server. It often includes a
+ * search form or links to help users navigate the site and provides a
+ * user-friendly message indicating the page is not available.
+ * 
  * @package THEMENAME
  * @since THEMENAME 0.0.0
  */
