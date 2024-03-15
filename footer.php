c2d1dd58f49aa5e0b16f83439f6e1ba4773c4ce5
foxtrotLi <58943731+foxtrotLi@users.noreply.github.com>
2024-03-14 22:52:09 -0400

    Add footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @package THEMENAME
+ * @since THEMENAME 0.0.0
+ */
+?>
+
+    <footer>
+        <!-- CONTENT HERE -->
+    </footer>
+
+<?php wp_footer();?>
+</body>
+</html>
