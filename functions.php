53ab89b37b02864010c2b3789f45d620d92479c3
foxtrotLi <58943731+foxtrotLi@users.noreply.github.com>
2025-07-04 14:35:02 -0400

    Disable Gutenberg editor

--- functions.php
+++ functions.php
@@ -61,6 +61,31 @@ add_action( 'wp_enqueue_scripts', 'themename_scripts' );
 
 
 /*
+ * ========================== GUTENBERG ==========================
+ * 
+ * Disable Gutenberg block editor and remove associated styles.
+ */
+
+// Disable Gutenberg on the back end.
+add_filter( 'use_block_editor_for_post', '__return_false' );
+
+// Disable Gutenberg for widgets.
+add_filter( 'use_widgets_block_editor', '__return_false' );
+
+
+add_action( 'wp_enqueue_scripts', function() {
+    // Remove CSS on the front end.
+    wp_dequeue_style( 'wp-block-library' );
+
+    // Remove Gutenberg theme.
+    wp_dequeue_style( 'wp-block-library-theme' );
+
+    // Remove inline global CSS on the front end.
+    wp_dequeue_style( 'global-styles' );
+}, 20 );
+
+
+/*
  * ================================ SMTP EMAILS ===============================
  * 
  * Use SMTP to send emails.
